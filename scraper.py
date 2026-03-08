21ba93ed94226296e1eafece0ee69d56889a09b7
cagpygs <cagpygs@gmail.com>
2026-03-08 06:22:37 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -13,12 +13,15 @@ import pandas as pd
 
 def setup_driver():
 
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
+    
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
 
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=chrome_options)
 
     wait = WebDriverWait(driver, 20)
 
