cfb973329f158aba3a2e18bf8cbb256b2917a963
cagpygs <cagpygs@gmail.com>
2026-03-08 06:21:45 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,16 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.devtools.v145.runtime import run_script
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 
 # DRIVER SETUP
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from selenium import webdriver
+
+
 
 def setup_driver():
 
